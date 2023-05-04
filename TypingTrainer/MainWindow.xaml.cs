2f53827dbac481166383ef9f0d942526f86c236a
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-05-04 15:18:34 +0300

    Add Typing Trainer draft

---
+++ TypingTrainer/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+// Typing Trainer (Draft)
+
+namespace TypingTrainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        DateTime start;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerTickProgress;
+            start = DateTime.Now;
+        }
+
+        private void TimerTickProgress(object sender, EventArgs e)
+        {
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                MessageBox.Show("You haven't completed entering text on time!");
+                progressBar.Value = progressBar.Minimum;
+            }
+            else { progressBar.Value++; }
+        }
+
+        private void StartBtnClick(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            secondsText.Content = Convert.ToString(DateTime.Now - start);
+        }
+    }
+}
