f4c37aa26df4598518f2ef415cecbb8ea7308cc9
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-06-03 23:37:44 +0300

    Add Color Picker exam project

---
+++ Exam Task - Color Picker/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// Task - Create Color Viewer application using bindings.
+
+namespace ColorViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel viewModel = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = viewModel;
+        }
+        private void CreateBtnClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.AddColor();
+        }
+
+        private void RemoveBtnClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.RemoveColor();
+        }
+    }
+}
---
+++ Exam Task - Color Picker/ViewModel.cs
@@ -0,0 +1,58 @@
+﻿using PropertyChanged;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System;
+using System.Drawing;
+
+// View Model
+
+namespace ColorViewer
+{
+    public partial class MainWindow
+    {
+        [AddINotifyPropertyChangedInterface]
+        public class ViewModel
+        {
+            private ObservableCollection<ColorModel> colors = new();
+            public IEnumerable<ColorModel> Colors => colors;
+
+            private ColorModel colorModel = new();
+
+            public ColorModel ColorModel
+            {
+                get { return colorModel; }
+                set { colorModel = value ?? new(); }
+            }
+
+            public ViewModel()
+            {
+                colors.Add(new ColorModel() { A = 112, B = 20, G = 80, R = 17 });
+                colors.Add(new ColorModel() { A = 86, B = 27, G = 172, R = 72 });
+                colors.Add(new ColorModel() { A = 50, B = 73, G = 11, R = 2 });
+                colors.Add(new ColorModel() { A = 120, B = 41, G = 70, R = 14 });
+                colors.Add(new ColorModel() { A = 38, B = 18, G = 39, R = 59 });
+            }
+
+            public void AddColor()
+            {
+                if (colors.Select(x => x.Color).Contains(ColorModel.Color))
+                {
+                    MessageBox.Show("This color already exists!");
+                }
+                else
+                {
+                    colors.Add(ColorModel);
+                }
+            }
+
+            public void RemoveColor()
+            {
+                colors.Remove(ColorModel);
+            }
+        }
+
+    }
+}
